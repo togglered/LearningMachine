17133308ed13d0f74056638c05a9b1aa4827eede
dariukhin <dariukhin@gmail.com>
2026-06-22 21:56:42 +0300

    chore: add CORS for frontend dev

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,6 +35,11 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS: list[str] = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 
 # Application definition
 
@@ -51,6 +56,7 @@ INSTALLED_APPS = [
     "apps.content",
     "apps.attempts",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "users.User"
@@ -58,6 +64,7 @@ AUTH_USER_MODEL = "users.User"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
